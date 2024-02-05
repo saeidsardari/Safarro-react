ec9877096bd5f68f816b2e77edf253e0e0189b84
unknown <saeidsardariii@gmail.com>
2024-02-05 12:53:17 +0330

    added err handling

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,29 +1,28 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import './TripList.css'
+import { useFetch } from "../hooks/useFetch"
+
 export default function TripList() {
-
-    const [trips, setTrips] = useState([])
     const [url, setUrl] = useState('http://localhost:3000/trips')
-
-    console.log(trips)
-
-useEffect(() => {
-    fetch(url)
-        .then(response => response.json())
-        .then(json => setTrips(json))
-},[url])
+    const {data : trips, isLoading, error} = useFetch(url)
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
+      {isLoading && <div>Loading Trips...</div>}
+      {error && <div>{error}</div>}
       <ul>
-        {trips.map(trip =>(
+        {trips && trips.map(trip =>(
             <li key={trip.id}>
                 <h3>{trip.title}</h3>
                 <p>{trip.price}</p>
             </li>
         ))}
       </ul>
+      <div className="filters">
+            <button onClick={()=>setUrl('http://localhost:3000/trips?location=IRAN')}>Iranian trips</button>
+            <button onClick={()=>setUrl('http://localhost:3000/trips')}>All trips</button>
+      </div>
     </div>
   )
 }
